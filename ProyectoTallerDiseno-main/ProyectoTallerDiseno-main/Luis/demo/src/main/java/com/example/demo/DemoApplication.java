d447c32a91b75c59fbc32daf40f1ffb905b44bfe
ludovic2711 <rendonrestrepol@gmail.com>
2023-10-15 15:07:33 -0500

    arreglar conexion a postgres

--- ProyectoTallerDiseno-main/ProyectoTallerDiseno-main/Luis/demo/src/main/java/com/example/demo/DemoApplication.java
+++ ProyectoTallerDiseno-main/ProyectoTallerDiseno-main/Luis/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SqlInitializationAutoConfiguration.class)
 public class DemoApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
